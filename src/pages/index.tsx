dbe684f65adab37092b9289158eed7a4c8faf875
Alexandre Monteiro <alexandre.monteiro.bec@gmail.com>
2021-05-16 17:58:32 -0300

    adding sign in page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,67 @@
+import { Button, Flex, Input, Stack, FormLabel, FormControl } from '@chakra-ui/react';
+
 export default function Home() {
   return (
-    <h1>Dashgo</h1>
+    <Flex 
+      w="100vw" 
+      h="100vh" 
+      align="center" 
+      justify="center"
+    >
+      <Flex 
+        as="form" 
+        w="100%" 
+        maxW="360" 
+        bg="gray.800" 
+        p="8" 
+        borderRadius="8"
+        flexDir="column"
+      >
+        <Stack spacing={4}>
+          <FormControl>
+            <FormLabel htmlFor="email">E-mail</FormLabel>
+
+            <Input 
+              name="email" 
+              id="email"
+              type="email" 
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{
+                bgColor: 'gray.900'
+              }}
+              size="lg"
+            />
+          </FormControl>
+
+          <FormControl>
+            <FormLabel htmlFor="password">Password</FormLabel>
+
+            <Input 
+              name="password"
+              id="password"
+              type="password" 
+              focusBorderColor="pink.500" 
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{
+                bgColor: 'gray.900'
+              }}
+              size="lg"
+            />
+          </FormControl>
+        </Stack>
+        
+        <Button 
+          type="submit" 
+          mt="6" 
+          colorScheme="pink" 
+          size="lg"
+        >
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
   )
 }
