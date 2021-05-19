90b196967e884e6857284efb2bbaa18de2a203b4
Alexandre <alexandre.monteiro.bec@gmail.com>
2021-05-18 21:31:56 -0300

    refactoring the Pagination component in small components

---
+++ src/components/Pagination/PaginationItem.tsx
@@ -0,0 +1,40 @@
+import { Button } from "@chakra-ui/react";
+
+interface PaginationItemProps {
+  number: number;
+  isCurrent?: boolean;
+}
+
+export function PaginationItem({ number, isCurrent = false }: PaginationItemProps) {
+  if (isCurrent) {
+    return (
+      <Button
+        size="sm"
+        fontSize="xs"
+        w="4"
+        colorScheme="pink"
+        disabled
+        _disabled={{
+          bg: 'pink.500',
+          cursor: 'default'
+        }}
+      >
+        {number}
+      </Button>
+    );
+  }
+
+  return (
+    <Button
+      size="sm"
+      fontSize="xs"
+      w="4"
+      bg="gray.700"
+      _hover={{
+        bg: 'gray.500'
+      }}
+    >
+      {number}
+    </Button>
+  );
+}
