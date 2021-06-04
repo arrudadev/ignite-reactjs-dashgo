1af6a492b336e6b4ff8b9f6da9aa3ffce15d1472
Alexandre <alexandre.monteiro.bec@gmail.com>
2021-06-04 00:29:22 -0300

    adding validation in create user form

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -1,5 +1,9 @@
 import Link from "next/link";
 
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
+
 import { 
   Box, 
   Button, 
@@ -15,7 +19,35 @@ import { Input } from "../../components/Form/Input";
 import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
 
+type CreateUserFormData = {
+  name: string;
+  email: string;
+  password: string;
+  password_confirmation: string;
+}
+
+const CreateUserFormSchema = yup.object().shape({
+  name: yup.string().required('Nome obrigatório'),
+  email: yup.string().required('E-mail obrigatório').email('E-mail inválido'),
+  password: yup.string().required('Senha obrigatória').min(6, 'No mínimo 6 caracteres'),
+  password_confirmation: yup.string().oneOf([
+    null, yup.ref('password')
+  ], 'As senhas precisam ser iguais'),
+});
+
 export default function CreateUser() {
+  const { register, handleSubmit, formState } = useForm({
+    resolver: yupResolver(CreateUserFormSchema)
+  });
+
+  const handleCreateUser: SubmitHandler<CreateUserFormData> = async (values) => {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    
+    console.log(values);
+  }
+
+  const { errors } = formState;
+
   return (
     <Box>
       <Header />
@@ -23,20 +55,51 @@ export default function CreateUser() {
       <Flex w="100%" my="6" maxW={1480} mx="auto" px="6">
         <Sidebar />
         
-        <Box flex="1" borderRadius={8} bg="gray.800" p={["6", "8"]} w="100%">
+        <Box 
+          as="form" 
+          flex="1" 
+          borderRadius={8} 
+          bg="gray.800" 
+          p={["6", "8"]} 
+          w="100%"
+          onSubmit={handleSubmit(handleCreateUser)}
+        >
           <Heading size="lg" fontWeight="normal">Criar  usuário</Heading>
 
           <Divider  my="6" borderColor="gray.700" />
 
           <VStack spacing={["6", "8"]}>
             <SimpleGrid minChildWidth={["180px", "240px"]} spacing={["6", "8"]} w="100%">
-              <Input name="name" label="Nome completo" />
-              <Input name="email" type="email" label="E-mail" />
+              <Input 
+                name="name" 
+                label="Nome completo"
+                error={errors.name}
+                {...register('name')}
+              />
+              <Input 
+                name="email" 
+                type="email" 
+                label="E-mail"
+                error={errors.email}
+                {...register('email')}
+              />
             </SimpleGrid>
 
             <SimpleGrid minChildWidth={["180px", "240px"]} spacing={["6", "8"]} w="100%">
-              <Input name="password" type="password" label="Senha" />
-              <Input name="password_confirmation" type="password" label="Confirmação da senha" />
+              <Input
+                name="password"
+                type="password"
+                label="Senha" 
+                error={errors.password}
+                {...register('password')}
+              />
+              <Input
+                name="password_confirmation"
+                type="password"
+                label="Confirmação da senha" 
+                error={errors.password_confirmation}
+                {...register('password_confirmation')}
+              />
             </SimpleGrid>
           </VStack>
 
@@ -45,7 +108,13 @@ export default function CreateUser() {
               <Link href="/users" passHref>
                 <Button colorScheme="whiteAlpha">Cancelar</Button>
               </Link>              
-              <Button colorScheme="pink">Salvar</Button>
+              <Button 
+                type="submit" 
+                colorScheme="pink"
+                isLoading={formState.isSubmitting}
+              >
+                Salvar
+              </Button>
             </HStack>
           </Flex>
         </Box>
